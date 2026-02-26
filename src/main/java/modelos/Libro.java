9bdce738e148571faec1371ad9a6d814a28d0bfb
Joseph Mateo Trespalacios Luque <jmateotrespalacios@unicesar.edu.co>
2026-02-26 18:11:54 -0500

    feat: crear clases libros con sus atributos y getters y setters

---
+++ src/main/java/modelos/Libro.java
@@ -0,0 +1,74 @@
+
+package modelos;
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int aniopublicacion;
+    private boolean disponible;
+
+    
+    public String getIsbn() {
+        return isbn;
+    }
+
+   
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+   
+    public String getTitulo() {
+        return titulo;
+    }
+
+   
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+   
+    public String getAutor() {
+        return autor;
+    }
+
+    
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+   
+    public String getGenero() {
+        return genero;
+    }
+
+    
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+   
+    public int getAniopublicacion() {
+        return aniopublicacion;
+    }
+
+   
+    public void setAniopublicacion(int aniopublicacion) {
+        this.aniopublicacion = aniopublicacion;
+    }
+
+   
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+   
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    
+}
+
