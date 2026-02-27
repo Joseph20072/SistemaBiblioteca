0fc3f1a3fdff278c4813c5ac7010dde9c38f50fd
Joseph Mateo Trespalacios Luque <jmateotrespalacios@unicesar.edu.co>
2026-02-27 07:11:57 -0500

    feat: LibroRepository creado correctamente con sistema CRUD

--- src/main/java/persistencia/LibroRepository.java
+++ src/main/java/persistencia/LibroRepository.java
@@ -1,8 +1,67 @@
 
 package persistencia;
+import java.util.ArrayList;
 import modelos.Libro;
 
 
 public class LibroRepository {
+    
+    private ArrayList<Libro> libros;
+    
+    public LibroRepository(){
+    this.libros = new ArrayList<>();
+    }
+    
+    public void crearlibro(Libro libro){
+    libros.add(libro);
+        System.out.println("Libro agregado correctamente");
+    }
+    
+    
+    public void listarlibros(){
+        if(libros.isEmpty()){
+            System.out.println("No hay libros registrados hasta el momento");
+        } else{
+        for(Libro l : libros){
+            System.out.println(l);
+        }
+      }
+    }
+    
+    
+    public Libro buscarlibro(String isbn){
+    for(Libro l : libros){
+    if(l.getIsbn().equals(isbn)){
+            return l;
+    }
+    }
+        System.out.println("Libro no encontrado");
+        return null;
+    }
+    
+    
+    public void modificarLibro(Libro libro) {
+        for (int i = 0; i < libros.size(); i++) {
+            if (libros.get(i).getIsbn().equals(libro.getIsbn())) {
+                libros.set(i, libro);
+                System.out.println("Libro modificado correctamente.");
+                return;
+            }
+        }
+        System.out.println("Libro no encontrado.");
+    }
+
+
+ public void eliminarLibro(String isbn) {
+        for (int i = 0; i < libros.size(); i++) {
+            if (libros.get(i).getIsbn().equals(isbn)) {
+                libros.remove(i);
+                System.out.println("Libro eliminado correctamente.");
+                return;
+            }
+        }
+        System.out.println("Libro no encontrado.");
+    }
+    
     
 }
